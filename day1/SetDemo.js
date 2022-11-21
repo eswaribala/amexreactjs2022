4591eab98a1ded88d5ce7c2646e4e040391cde32
eswaribala <parameswaribala@gmail.com>
2022-11-21 12:21:41 +0530

    updated

---
+++ day1/SetDemo.js
@@ -0,0 +1,26 @@
+//set of loan accounts
+
+var loanSet=new Set();
+
+for(let i=0;i<5;i++){
+    loanSet.add((Math.random()*100000).toFixed(0))
+}
+//loan Set
+console.log("Loan Set...........")
+loanSet.forEach(x=>console.log(x));
+
+var fdSet=new Set();
+
+for(let i=0;i<5;i++){
+    fdSet.add((Math.random()*100000).toFixed(0))
+}
+
+console.log("FD Set...........")
+fdSet.forEach(x=>console.log(x));
+
+
+//union
+var loanFDSet=new Set([...loanSet,...fdSet])
+
+console.log("Loan and FD Set...........")
+loanFDSet.forEach(x=>console.log(x));
