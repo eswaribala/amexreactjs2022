1103265445c13881a86c6772eb113b19bd366934
eswaribala <parameswaribala@gmail.com>
2022-11-22 14:39:49 +0530

    updated

--- amexstream/src/components/Molecules/header.js
+++ amexstream/src/components/Molecules/header.js
@@ -4,14 +4,17 @@ import React from "react";
 import Logo from "../Atoms/logo";
 import Banner from "../Atoms/banner";
 import './header.css'
-const Header=(props)=>(
+const Header=(props)=> {
 
-    <div className="App-header">
-        <Logo/>
-        <h1 className="multicolortext">Video Streaming</h1>
-        <h2>{props.pick.toLocaleTimeString()}</h2>
-        <Banner/>
-    </div>
-)
+    return(
+
+        <div className="App-header">
+            <Logo/>
+            <h1 className="multicolortext">Video Streaming</h1>
+            <h2>{props.pick.toLocaleTimeString()}</h2>
+            <Banner/>
+        </div>
+    )
+}
 
 export default Header;
