dc65d41097481795f5f50893bb007584e1d2fec2
eswaribala <parameswaribala@gmail.com>
2022-11-23 13:25:47 +0530

    updated

--- amexstream/src/components/Molecules/userMenu.js
+++ amexstream/src/components/Molecules/userMenu.js
@@ -1,9 +1,9 @@
 import React, {Component} from "react";
 import { TieredMenu } from 'primereact/tieredmenu';
 import {items} from '../../assets/data/userMenuData'
-
+import './userMenu.css'
 
 
 export const UserMenu=()=>(
-    <TieredMenu model={items} />
+    <TieredMenu model={items} className="menu" />
 )
--- amexstream/src/index.js
+++ amexstream/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './App';
 import reportWebVitals from './reportWebVitals';
-import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/themes/arya-orange/theme.css" //theme
 import "primereact/resources/primereact.min.css";                  //core css
 import "primeicons/primeicons.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
