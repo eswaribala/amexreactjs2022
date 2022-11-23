116fe8673b697809a0e4959542ba23d2bdbf6efe
eswaribala <parameswaribala@gmail.com>
2022-11-23 15:09:19 +0530

    updated

--- amexstream/src/components/Organisms/landingPage.js
+++ amexstream/src/components/Organisms/landingPage.js
@@ -4,12 +4,15 @@ import React from "react";
 import Header from "../Molecules/header";
 import {LoginForm} from "../Molecules/loginForm";
 window.sessionStorage.setItem("loggedIn","false");
-const LandingPage=(props)=>(
+const LandingPage=(props)=>{
 
-    <div>
-       <Header pick={props.pick}/>
-       <LoginForm/>
-    </div>
-)
+    window.sessionStorage.setItem("upload","no");
+    return(
+        <div>
+            <Header pick={props.pick}/>
+            <LoginForm/>
+        </div>
+    )
+}
 
 export default LandingPage;
