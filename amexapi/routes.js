042b9b003becc8d14a6c7b14c0249e88029f4c36
eswaribala <parameswaribala@gmail.com>
2022-11-23 16:29:37 +0530

    updated

--- amexapi/routes.js
+++ amexapi/routes.js
@@ -1,11 +1,37 @@
 const users = require("./usercontroller.js");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+
+//const upload = multer({ dest: "uploads/" });
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public");
+    },
+    filename: (req, file, cb) => {
+        const ext = file.mimetype.split("/")[1];
+        cb(null, `files/admin-${file.fieldname}-${Date.now()}.${ext}`);
+    },
+});
+
 module.exports = app => {
 
     const users= require("./usercontroller.js");
 
     var router = require("express").Router();
+    // Multer Filter
+    const multerFilter = (req, file, cb) => {
+        var ext=file.mimetype.split("/")[1];
+
+        if ((ext === "mp4")||(ext === 'ogg')||(ext==='ogv'))
+           {
+            cb(null, true);
+        } else {
+            cb(new Error("Not a Video File!!"), false);
+        }
+    };
+    const upload = multer({
+        storage: multerStorage,
+        fileFilter: multerFilter,
+    });
     var type = upload.single('amexvideo');
 
     router.post("/",users.create);
