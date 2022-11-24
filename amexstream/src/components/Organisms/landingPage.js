f1ff9d728f407197842cea2d53c12010ada4e70a
eswaribala <parameswaribala@gmail.com>
2022-11-24 14:49:26 +0530

    updated

--- amexstream/src/components/Organisms/landingPage.js
+++ amexstream/src/components/Organisms/landingPage.js
@@ -6,14 +6,14 @@ import CryptoJS from 'crypto-js'
 const LandingPage=(props)=>{
     window.localStorage.setItem("loggedIn","false");
     window.localStorage.setItem("adminLoggedIn","false")
-    var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
+   /* var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123').toString();
     console.log(ciphertext);
     // Decrypt
     var bytes  = CryptoJS.AES.decrypt(ciphertext, 'secret key 123');
     var originalText = bytes.toString(CryptoJS.enc.Utf8);
 
     console.log(originalText); // 'my message'
-
+*/
     return(
         <div>
             <Header pick={props.pick}/>
